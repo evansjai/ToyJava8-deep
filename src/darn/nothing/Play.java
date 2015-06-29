6aa92f572d1a3795525bf9752e7ab5cbf2033723
evansjai <jai.a.evans@gmail.com>
2015-06-28 20:06:08 -0400

    First flavor check and init hubbbub

---
+++ src/darn/nothing/Play.java
@@ -0,0 +1,14 @@
+package darn.nothing;
+public class Play {
+
+    public Play(){
+        //do nothing
+
+    }
+
+    public void didNothing(){
+        System.out.println("Hello Worlds");
+    }
+
+
+}
---
+++ test/darn/nothing/PlayTest.java
@@ -0,0 +1,19 @@
+package darn.nothing;
+
+public class PlayTest {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testDidNothing() throws Exception {
+
+    }
+}
